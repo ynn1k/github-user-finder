813400cc0e28e09a0df1cd95ad9fcb9bd2514595
Yannik Hewlik <hi@yannik.dev>
2019-10-22 21:45:01 +0200

    [FEATURE] display profile data; ability to show/remove an alert

---
+++ src/js/ui.js
@@ -0,0 +1,71 @@
+class Ui {
+    constructor() {
+        this.profile = document.querySelector('#profile');
+    }
+
+    //displays profile
+    showProfile(user) {
+        this.profile.innerHTML = `
+            <div class="card card-body mb-3">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="${user.avatar_url}" alt="" class="img-fluid mb-2 rounded-lg">
+                        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-2">View Profile</a>
+                    </div>
+                    <div class="col-md-9">
+                        <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+                        <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+                        <span class="badge badge-success">Follower: ${user.followers}</span>
+                        <span class="badge badge-info">Following: ${user.following}</span>
+                        <br>
+                        <br>
+                        <ul class="list-group">
+                            <li class="list-group-item">Company: ${user.company}</li>
+                            <li class="list-group-item">Website: ${user.blog}</li>
+                            <li class="list-group-item">Location: ${user.location}</li>
+                            <li class="list-group-item">Member since: ${user.created_at}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <h3 class="page-heading mb-3">Latest Repos</h3>
+            <div id="repos"></div>
+        `;
+    }
+
+    //shows an alert message
+    showAlert(message, className) {
+        //clear remainig alert
+        this.clearAlert();
+        //create div
+        const alertElement = document.createElement('div');
+        //add classes
+        alertElement.className = className;
+        //add text TODO: innerText????
+        alertElement.appendChild(document.createTextNode(message));
+        //get parent
+        const alertParent = document.querySelector('.searchContainer');
+        //get search box
+        const search = document.querySelector('.search');
+        //insert alert beforesearch box
+        alertParent.insertBefore(alertElement, search);
+        //clear alert after 3sec
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000)
+    }
+
+    //remove alert
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if(currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    //clears profile
+    clearProfile() {
+        this.profile.innerHTML = '';
+    }
+}
