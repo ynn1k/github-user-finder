e68e3f15df723d256f074e66f7f5ff76aef463ea
Yannik Hewlik <hi@yannik.dev>
2019-10-22 21:43:59 +0200

    [FEATURE] event listener to start fetching data from the api

---
+++ src/js/_script.js
@@ -0,0 +1,30 @@
+//init GitHub
+const github = new GitHub;
+//init Ui
+const ui = new Ui;
+
+//search input
+const searchUser = document.querySelector('#searchUser');
+
+//search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    //get input
+    const userText = e.target.value;
+
+    if (userText !== '') {
+        //make http call
+        github.getUser(userText)
+            .then(data => {
+                if(data.profile.message === 'Not Found') {
+                    //show alert
+                    ui.showAlert('User not found', 'alert alert-danger');
+                } else {
+                    //show profile
+                    ui.showProfile(data.profile);
+                }
+            });
+    } else {
+        //clear profile
+        ui.clearProfile();
+    }
+});
